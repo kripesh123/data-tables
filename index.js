3613d2bbfa26266529f9d32117900ee64d3ae933
kripesh123 <kripeshbista3@gmail.com>
2017-12-26 14:34:40 +0545

    basic initialisation

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#customers').DataTable({
+		/*"paging":   true,
+        "ordering": true,
+        "info":     true
+        //order particular column
+        "order":[[0,"asc"]]*/
+	});
+})
