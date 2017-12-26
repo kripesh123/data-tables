7795d808d7ce63c9e223f776d379bef9ce16e92a
kripesh123 <kripeshbista3@gmail.com>
2017-12-26 15:26:19 +0545

    Setting defaults

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+$.extend( true, $.fn.dataTable.defaults, {
+    "searching": false
+} );
+
 $(document).ready(function(){
 	
-	$('#customers').DataTable({
-		"language": {
-            "url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/German.json"
-        }
-	});
+	$('#customers').DataTable({});
 
 });
 		
